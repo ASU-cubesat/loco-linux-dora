07fff6a5f42c5a838e97c1d9d7a75fe8f5611d3f
Judd D. Bowman <judd.bowman@asu.edu>
2022-07-05 17:01:23 -0700

    Cleaned up rootfs_overlay and setup.sh

--- setup.sh
+++ setup.sh
@@ -1,43 +1,97 @@
 #!/bin/sh
 
-set -e -x
+#set -e -x
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+cd $SCRIPTPATH
 
 buildroot="buildroot-2022.02.1"
 buildroot_downloads="https://buildroot.uclibc.org/downloads"
+buildroot_dir="$SCRIPTPATH/../$buildroot"
+buildroot_make_args=
 
-external_dir="$(pwd)"
-buildroot_dir="$external_dir/../$buildroot"
-
-download=
-version=
 board=
+download=
+commit=
+version=
+localversion_string=
+localversion_file=$SCRIPTPATH/linux-localversion.config
+
+
+######################################
+### Define the help message        ###
+######################################
+help_message="
+  Configures buildroot to use this external tree by calling
+  'make' from the buildroot directory (assumed to be in the
+  parent directory) with the BR2_EXTERNAL flag set to this
+  path and the board name set to the name specified using
+  the -b option described below.  Buildroot will remember
+  these settings until its 'make distclean' is called.
+
+  After running setup.sh, change to the buildroot directory
+  and execute 'make' (no command line options required) to
+  start the build.
+
+  Syntax: ./setup.sh [-b|c|d|h|v]
+  options:
+   -b name    (required) Specify board name, e.g. pumpking-mbmb2
+   -c         Add the current git commit hash to the linux kernel
+              local version info.  Overwrites the file:
+              ./linux-localversion.config.  If used with -v, both
+              will be added to the version info.
+   -d         Download and unzip buildroot to the parent directory
+   -h         Print this help
+   -v text    Add a custom version string to the linux kernel
+              local version info (must be shorter than 54 
+              characters).  Overwrites the file:
+              ./linux-localversion.config.  If used with -c, both
+              will be added to the version info.
+"
 
 ######################################
 ### Parse the command line options ###
 ######################################
-while getopts "b:dv:" option
+while getopts ":b:cdhv:" option
 do
     case $option in
-        b | --board)
+        b)
             board=$OPTARG
             ;;
-        d | --download)
+        c)
+            commit=`git rev-parse --verify HEAD | head -c 8`
+            ;;
+        d)
             download=1
             ;;
-        v | --version)
+        h)
+            echo "$help_message"
+            exit 0
+            ;;
+        v)
             version=$OPTARG
             ;;
+        :)
+            echo "Error:  Missing argument for -${OPTARG}." >&2
+            echo "$help_message" >&2
+            exit 1
+            ;;
         \?)
+            echo "Error:  Unrecognized argument ${OPTARG}." >&2
+            echo "$help_message" >&2
             exit 1
             ;;
     esac
 done
 
-echo $external_dir
-echo $buildroot_dir
-echo $board
-echo $download
-echo $version
+if [ -z "$board" ]
+then
+    echo "Error:  Argument -b must be provided." >&2
+    echo "$help_message" >&2
+    exit 1
+fi
 
 #############################################################
 ### Download and install the buildroot package (optional) ###
@@ -46,35 +100,44 @@ if ! ${download}
 then
     cd ..
 
-    echo "Downloading buildroot..."
+Downloading buildroot..."
     wget $buildroot_downloads/$buildroot.tar.gz 
 
-    echo "Unpacking buildroot..."
+Unpacking buildroot..."
     tar xzf $buildroot.tar.gz
 
-    echo "Removing temporary files..."
+Removing temporary files..."
     rm $buildroot.tar.gz
 
-    echo "Download complete"
+Download complete"
 fi
 
-####################
-### Add git hash ###
-####################
+#####################################
+### Make the local version string ###
+#####################################
 
 
-################################
-### Specify a version number ###
-################################
+#  Add the version number specified on the command line
+if [ ! -z "$version" ]
+then
+    localversion_string="${localversion_string}-${version}"
+fi
 
+# Add the latest git commit for this repository (not the linux kernel source tree)
+if [ ! -z "$commit" ]
+then
+    localversion_string="${localversion_string}-${commit}"
+fi
 
-#latest_tag=`git tag --sort=-creatordate | head -n 1`
-#sed -i "s/0.0.0/$latest_tag/g" board/pumpkin-mbm2/linux.config
+## This file created automatically by setup.sh" > $version_file
+if [ ! -z "$localversion_string" ]
+then
+    echo "Creating ./linux-localversion.config with CONFIG_LOCALVERSION=\"${localversion_string}\""
+    echo "# This file auto created by ./setup.sh on " `date -u` > $localversion_file
+    echo "CONFIG_LOCALVERSION=\"${localversion_string}\"" >> $localversion_file
+fi
 
-#echo "Building $latest_tag"
-
-#cd .. #cd out of the kubos-linux-build directory
-
+#cd ..
 #git init 
 #git config --local url."https://".insteadOf git://
 
@@ -88,14 +151,21 @@ fi
 # time you call make.
 echo "Adding this tree to buildroot for board: $board..."
 cd $buildroot_dir
-make BR2_EXTERNAL="${external_dir}" ${board}_defconfig
+
+# Add the path to this external tree
+buildroot_make_args="${buildroot_make_args} BR2_EXTERNAL=\"${SCRIPTPATH}\""
+
+# End the buildroot make arguments with the default configuration file
+buildroot_make_args="${buildroot_make_args} ${board}_defconfig"
+
+# Call make so buildroot remembers these settings
+echo "`pwd`"
+echo "Calling: make ${buildroot_make_args}"
+make $buildroot_make_args
 
 ##################
 ### End tidily ###
 ##################
-
-# Return the original directory and end
-cd $external_dir
 echo "Done setting up"
 
 
